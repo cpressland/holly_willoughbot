40905cfe3152f6a56ae053f369031fb6a0814c4c
Chris Pressland <mail@cpressland.io>
2025-01-17 18:04:08 +0700

    Fix for Piccolo Migrations not found inside container context

--- holly_willoughbot/piccolo_app.py
+++ holly_willoughbot/piccolo_app.py
@@ -1,10 +1,12 @@
 """Import all of the Tables subclasses in your app here, and register them with the APP_CONFIG."""
 
+from pathlib import Path
+
 from piccolo.conf.apps import AppConfig, table_finder
 
 APP_CONFIG = AppConfig(
     app_name="holly_willoughbot",
-    migrations_folder_path="holly_willoughbot/migrations",
+    migrations_folder_path=Path(__file__).parent / "migrations",
     table_classes=table_finder(modules=["holly_willoughbot.tables"], exclude_imported=True),
     migration_dependencies=[],
     commands=[],
